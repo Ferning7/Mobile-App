3c5f2eb6308855598da851109067946fe018ae01
Matheus Fernandes <matheusfffcorrea@gmail.com>
2025-11-10 09:08:57 -0300

    Style: adicionado arquivo de stylesheets para usar cores predefinidas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,25 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
+import { colors } from "../styles/colors"
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> Hello React Native</Text>
+            <Text style={styles.title}> Hello React Native</Text>
+            <Text style={styles.title}> Hello React Native</Text>
 
-export default function Index (){
-    return(
-        <Text> Hello React Native</Text>
-        
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    title: {
+        color: "red",
+        fontSize: 22,
+    }
+})
