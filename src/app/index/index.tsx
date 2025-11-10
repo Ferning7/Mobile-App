2e203c011b86682dab42bc2e47ee834acd9e50ad
Matheus Fernandes <matheusfffcorrea@gmail.com>
2025-11-10 09:44:40 -0300

    Feat: estilizações adicionadas

---
+++ src/app/index/index.tsx
@@ -0,0 +1,13 @@
+import { Image, View } from "react-native"
+import { styles } from "./styles"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.pnh")}  style={styles.logo}/>
+            </View>
+        </View>
+    )
+}
+
